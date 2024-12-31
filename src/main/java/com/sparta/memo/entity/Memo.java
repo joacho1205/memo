13b8b98ab9b1671c5418e38cddce9e1026f71909
joacho <joacho1205@gmail.com>
2024-12-31 20:17:51 +0900

    feat: Memo 엔티티 생성

---
+++ src/main/java/com/sparta/memo/entity/Memo.java
@@ -0,0 +1,13 @@
+package com.sparta.memo.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class Memo {
+
+    private Long id;
+    private String title;
+    private String contents;
+}
