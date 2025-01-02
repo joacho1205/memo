a83c9fc5c485ec39f8e64a4fa797f3f2440c37a6
joacho <joacho1205@gmail.com>
2025-01-02 23:36:32 +0900

    feat: MemoResponseDto 생성

--- src/main/java/com/sparta/memo/dto/MemoResponseDto.java
+++ src/main/java/com/sparta/memo/dto/MemoResponseDto.java
@@ -3,7 +3,10 @@ package com.sparta.memo.dto;
 import lombok.Getter;
 
 @Getter
-public class MemoRequestDto {
+public class MemoResponseDto {
+
+    private Long id;
     private String title;
     private String contents;
+
 }
