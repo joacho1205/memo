286d6c78c631e6e4dd382dd251ee1cf31984ede4
joacho <joacho1205@gmail.com>
2025-01-13 19:48:20 +0900

    Fix: MemoController

--- src/main/java/com/sparta/memo/controller/MemoController.java
+++ src/main/java/com/sparta/memo/controller/MemoController.java
@@ -1,10 +1,18 @@
 package com.sparta.memo.controller;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.web.bind.annotation.RestController;
+
+import com.sparta.memo.entity.Memo;
 
 @RestController
 public class MemoController {
 
+	private final Map<Long, Memo> memoList = new HashMap<>();
+
+	
 
 
 }
